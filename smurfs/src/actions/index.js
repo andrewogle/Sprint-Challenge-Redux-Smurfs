1fc5b3705f2f875cd80a676707b0b687a00eb337
Andrew <andrewryanogle@gmail.com>
2019-02-09 11:05:37 -0800

    finished reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,16 @@
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const FETCH_SMURFS = "FETCH_SMURFS";
+export const FETCHING_SMURFS = "FETCHING_SMURFS";
+export const ADD_SMURF = "ADD_SMURF";
+export const ADDING_SMURF = "ADDING_SMURF";
+export const DELETE_SMURF = "DELETE_SMURF";
+export const DELETING_SMURF = "DELETING_SMURF";
+export const ERROR = "ERROR"
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +21,54 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = ()=>{
+  return dispatch => {
+    dispatch({type: FETCHING_SMURFS});
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response =>{
+        dispatch({
+          type: FETCH_SMURFS,
+          payload: response.data
+        });
+      })
+      .catch(error =>{
+        dispatch({
+          type: ERROR,
+          payload: error
+        })
+      })
+  }
+}
+
+export const addSmurf = (smurf) => {
+  const newSmurf = axios.post('http://localhost:3333/smurfs',smurf)
+  return dispatch =>{
+    dispatch({type: ADDING_SMURF});
+
+    newSmurf.then(({data})=>{
+      dispatch({
+        type: ADDING_SMURF,
+        payload: data
+      });
+    })
+    .catch(error =>{
+      dispatch({
+        type: ERROR,
+        payload: error
+      });
+    })
+  }
+}
+
+export const deleteSmurf = (id) => dispatch =>{
+  dispatch({type: DELETING_SMURF});
+  axios
+    .delete('http://localhost:3333/smurfs')
+    .then( () =>{
+      dispatch({type: DELETE_SMURF, id})
+    })
+    .catch(error =>{
+      dispatch({type:ERROR, payload: error});
+    })
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
+import {FETCH_SMURFS, FETCHING_SMURFS, ADD_SMURF, DELETE_SMURF, DELETING_SMURF, ERROR, ADDING_SMURF} from '../actions/index';
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this*/
 const initialState = {
@@ -22,5 +22,41 @@ const initialState = {
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
 export const smurfReducer =(state = initialState,action) =>{
-
+  switch(action.type){
+    case FETCH_SMURFS:
+      return{
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false
+      }
+      case FETCHING_SMURFS:
+        return{
+          ...state,
+          fetchingSmurfs: true
+        }
+      case ADD_SMURF:
+      return{
+        ...state,
+        smurfs: action.payload,
+        addingSmurf: true
+      }
+      case ADDING_SMURF:
+      return{
+        ...state,
+        addingSmurf: true
+      }
+      case DELETE_SMURF:
+      return {
+        ...state,
+        deletingSmurf: false,
+        smurfs: state.smurfs.filter(smurf => smurf.id !== action.id)
+      }
+      case DELETING_SMURF:
+      return{
+        ...state,
+        deletingSmurf: true
+      }
+      default:
+      return state;
+  }
 }
